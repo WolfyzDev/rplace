9ce39878a93407e2e41a4bbf3e97a8990973cb6d
WolfyzDev <wolfyzpro@gmail.com>
2022-04-23 18:43:34 +0200

    Passage à la version 1.5

--- templates/sauvegarde_etape_3/public/javascript/main.js
+++
@@ -1,6 +0,0 @@
-import webSocket from "./webSocket.js";
-
-webSocket();
-
-// Si cela ne fonctionne pas correctement : 
-// const socket = io();
--- templates/sauvegarde_etape_3/public/javascript/webSocket.js
+++
@@ -1,9 +0,0 @@
-const startWebSocket = () => {
-    const socket = io();
-
-        socket.on('update-pixel-data', (pixelData) => {
-            console.table("pixelData", pixelData);
-        });
-};
-
-export default startWebSocket
