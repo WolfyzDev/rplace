64df01bb9396e199982d50bd85e53647b9642198
WolfyzDev <wolfyzpro@gmail.com>
2022-04-23 12:47:50 +0200

    Création generatePixelData.js in src (création de la table en backend) + add in templates

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import generatePixelData from './src/generatePixelData.js';
+
+const pixelData = generatePixelData({
+    // Taille donnée pour les tests. A retirer à la fin pour prise auto. 600 X 900
+    pixelSize: 50,
+    width: 300,
+    height: 300,
+});
+console.table(pixelData);
 
 const app = express();
 
---
+++ templates/generatePixelData.js
@@ -0,0 +1,35 @@
+// Génération de la table des pixels data (tableau) - Sauvegarde #1
+
+// Fichier 'src/generatePixelData.js'
+const generatePixelData = ({
+    pixelSize = 10,
+    width = 900, 
+    height = 600
+} = {} ) => {
+    const colMax = width / pixelSize;
+    const rowMax = height / pixelSize;
+
+    const pixelData = [];
+
+    for (let rowIndex = 0; rowIndex < rowMax; rowIndex++) {
+        const row = [];
+        for (let colIndex = 0; colIndex < colMax; colIndex++) {
+            row.push(null);
+        }
+        pixelData.push(row);
+    }
+    return pixelData;
+};
+
+export default generatePixelData
+
+// Partie ajoutée dans index.js
+import generatePixelData from './src/generatePixelData.js';
+
+const pixelData = generatePixelData({
+    // Taille donnée pour les tests. A retirer à la fin pour prise auto. 600 X 900
+    pixelSize: 50,
+    width: 300,
+    height: 300,
+});
+console.table(pixelData);
